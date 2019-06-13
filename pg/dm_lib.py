033e930f0628184bcea53f8fbe6338bb92df3404
Francesco De Carlo <decarlof@gmail.com>
2019-06-12 20:39:07 -0500

    clean printout

--- pg/dm_lib.py
+++ pg/dm_lib.py
@@ -14,16 +14,16 @@ import logging
 
 
 def scp(global_PVs, variableDict):
-    info('  *** start scp')
+    info('      *** start scp')
     fname_origin = global_PVs['HDF1_FullFileName_RBV'].get(as_string=True)
     p = pathlib.Path(fname_origin)
     
     fname_destination = variableDict['RemoteAnalysisDir'] + p.parts[-3] + '/' + p.parts[-2] + '/'
-    info('  *** *** origin: %s' % fname_origin)
-    info('  *** *** destination: %s' % fname_destination)
+    info('           *** origin: %s' % fname_origin)
+    info('           *** destination: %s' % fname_destination)
 
     err = os.system('scp -q ' + fname_origin + ' ' + fname_destination + '&')
     if (err == 0):
-        info('  *** start scp: Done!')
+        info('      *** start scp: Done!')
     else:
-        error('  *** scp error: check that destination directory exists at %s' % (fname_destination))
+        error('      *** scp error: check that destination directory exists at %s' % (fname_destination))
