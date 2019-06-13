6a9aae8144fe71050d2f3cbe73b9c923ba779b0c
Francesco De Carlo <decarlof@gmail.com>
2019-06-12 20:05:38 -0500

    add logs to dm_lib

--- pg/dm_lib.py
+++ pg/dm_lib.py
@@ -15,15 +15,15 @@ import logging
 
 
 def scp(global_PVs, variableDict):
-#    log_lib.Logger(variableDict['LogFileName']).info('  *** start scp')
-#    Logger(variableDict['LogFileName']).info('  *** start scp')
+    info('  *** start scp')
+    info('  *** start scp')
     fname_origin = global_PVs['HDF1_FullFileName_RBV'].get(as_string=True)
     p = pathlib.Path(fname_origin)
     
     fname_destination = variableDict['RemoteAnalysisDir'] + p.parts[-3] + '/' + p.parts[-2] + '/'
-#    log_lib.Logger(variableDict['LogFileName']).info('  *** *** origin: %s' % fname_origin)
-#    log_lib.Logger(variableDict['LogFileName']).info('  *** *** destination: %s' % fname_destination)
-#    Logger(variableDict['LogFileName']).info('  *** *** origin: %s' % fname_origin)
+    info('  *** *** origin: %s' % fname_origin)
+    info('  *** *** destination: %s' % fname_destination)
+    info('  *** *** origin: %s' % fname_origin)
     info('  *** *** destination: %s' % fname_destination)
 
     err = os.system('scp -q ' + fname_origin + ' ' + fname_destination + '&')
